aabfd18f3703736464e6326be56069e5f27da0d7
root <root@DESKTOP-SGVTTE3.localdomain>
2022-03-11 20:54:58 +0300

    Fix

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
-# create a file in /tmp with specific requirements
+# create a file in /tmp
 
 file { 'school':
-  path    => '/tmp/holberton',
+  path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
