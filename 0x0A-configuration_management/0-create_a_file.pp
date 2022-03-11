d39d7243908a2a93045b1134f1cbe423c9bfb6ac
root <root@DESKTOP-SGVTTE3.localdomain>
2022-03-11 20:52:38 +0300

    Fix

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
-# create a new file in the /tmp directory
+# create a file in /tmp with specific requirements
 
 file { 'school':
-       path     => '/tmp/school',
-       permission     => '0744',
-       owner    => 'www-data',
-       group    => 'www-data',
-       content  => 'I love Puppet',
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
