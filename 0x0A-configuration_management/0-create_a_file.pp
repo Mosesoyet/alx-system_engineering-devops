8e8896a53efce3f12a6c7261dcb0cdf2f3d06aee
root <root@DESKTOP-SGVTTE3.localdomain>
2022-03-11 20:48:35 +0300

    First commit

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# create a new file in the /tmp directory
+
+file { 'school':
+       path     => '/tmp/school',
+       mode     => '0744',
+       owner    => 'www-data',
+       group    => 'www-data',
+       content  => 'I love Puppet',
+}
