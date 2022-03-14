15cd4fd6fb82555c0498cba8d5d85a8377683dd1
root <root@DESKTOP-SGVTTE3.localdomain>
2022-03-14 20:58:28 +0300

    Use puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+# Use puppet to setup configuration file
+
+include stdlib
+file_line { 'Declare identity file':
+       path    => '/etc/ssh/ssh_config',
+       line    => '   IdentityFile ~/.ssh/school',
+       replace => true,
+}
+
+file_line { 'Turn off passwd auth':
+     path    => '/etc/ssh/ssh_config',
+     line    => '    PasswordAuthentication no',
+     replace => true,
+}
